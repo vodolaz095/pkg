3b84c230dff146580c02b16bbb614d3aa9edd2f0
Anatolij Ostroumov <anatolij@vodolaz095.ru>
2025-01-07 22:46:19 +0300

    refactor tracing

---
+++ tracing/all.go
@@ -0,0 +1,60 @@
+package tracing
+
+import (
+	"fmt"
+
+	"go.opentelemetry.io/otel/attribute"
+)
+
+type Config struct {
+	// Protocol sets how we send spans to jaeger - over udp or over http
+	Protocol string `yaml:"protocol" validate:"required, oneof=udp http UDP HTTP"`
+
+	/*
+		Configuration for Jaeger exporter to send spans to a Jaeger agent over compact thrift protocol over UDP
+	*/
+
+	// Host - sets hostname of Jaeger agent, overrides environment value of OTEL_EXPORTER_JAEGER_AGENT_HOST.
+	// Default value is `localhost`
+	Host string `yaml:"host" validate:"hostname"`
+	// Port - sets port where Jaeger agent listens, overrides environment value of OTEL_EXPORTER_JAEGER_AGENT_PORT.
+	// Default value is `6831`
+	Port string `yaml:"port" validate:"gte=0,lte=65535"`
+
+	/*
+		Configuration for Jaeger exporter to use full URL to the Jaeger HTTP Thrift collector.
+	*/
+
+	// Endpoint is the URL for the Jaeger collector that spans are sent to,
+	// overrides value of environment variable OTEL_EXPORTER_JAEGER_ENDPOINT.
+	// Default value is `http://localhost:14268/api/traces`
+	Endpoint string `yaml:"endpoint" validate:"url"`
+	// Username used for basic authorization to access Jaeger collector. Setting value overrides environment
+	// variable OTEL_EXPORTER_JAEGER_USER. Default is empty
+	Username string `yaml:"username"`
+	// Password  used for basic authorization to access Jaeger collector. Setting value overrides environment
+	// variable OTEL_EXPORTER_JAEGER_PASSWORD. Default is empty
+	Password string `yaml:"password"`
+
+	// Ratio sets percent of spans to record, where 1 - means every span is recorded, 0 - no spans recorded and .05 means only 5% of spans are recorded
+	Ratio float64 `yaml:"ratio" validate:"required,lte=1,gte=0"`
+}
+
+func Start(cfg Config, extraAttributes ...attribute.KeyValue) (err error) {
+	switch cfg.Protocol {
+	case "udp", "UDP":
+		return ConfigureUDP(UDPConfig{
+			Host:  cfg.Host,
+			Port:  cfg.Port,
+			Ratio: cfg.Ratio,
+		}, extraAttributes...)
+	case "http", "HTTP":
+		return ConfigureHTTP(HTTPConfig{
+			Endpoint: cfg.Endpoint,
+			Username: cfg.Username,
+			Password: cfg.Password,
+		}, extraAttributes...)
+	default:
+		return fmt.Errorf("unknowwn protocol: %s", cfg.Protocol)
+	}
+}
--- tracing/compact_jaeger_thrift_udp.go
+++ tracing/compact_jaeger_thrift_udp.go
@@ -1,17 +1,20 @@
 package tracing
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/rs/zerolog/log"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/jaeger"
 )
 
 type UDPConfig struct {
-	Endpoint string  `yaml:"endpoint" validate:"hostname_port"`
-	Ratio    float64 `yaml:"ratio" validate:"lte=1,gte=0"`
+	// Host - sets hostname of Jaeger agent, overrides environment value of OTEL_EXPORTER_JAEGER_AGENT_HOST.
+	// Default value is `localhost`
+	Host string `yaml:"host" validate:"hostname"`
+	// Port - sets port where Jaeger agent listens, overrides environment value of OTEL_EXPORTER_JAEGER_AGENT_PORT.
+	// Default value is `6831`
+	Port string `yaml:"port" validate:"gte=0,lte=65535"`
+	// Ratio sets percent of spans to record, where 1 - means every span is recorded, 0 - no spans recorded and .05 means only 5% of spans are recorded
+	Ratio float64 `yaml:"ratio" validate:"required,lte=1,gte=0"`
 }
 
 func ConfigureUDP(cfg UDPConfig, extraAttributes ...attribute.KeyValue) (err error) {
@@ -19,23 +22,22 @@ func ConfigureUDP(cfg UDPConfig, extraAttributes ...attribute.KeyValue) (err err
 		log.Debug().Msgf("Tracing disabled")
 		return nil
 	}
-	if cfg.Endpoint == "" {
-		host := loadFromEnv("OTEL_EXPORTER_JAEGER_AGENT_HOST", "localhost")
-		port := loadFromEnv("OTEL_EXPORTER_JAEGER_AGENT_PORT", "6831")
-		log.Debug().Msgf("Sending traces using compact jaeger thrift protocol via udp into %s:%s.", host, port)
-		exp, err = jaeger.New(jaeger.WithAgentEndpoint())
+	var host, port string
+	var opts []jaeger.AgentEndpointOption
+	if cfg.Host != "" {
+		opts = append(opts, jaeger.WithAgentHost(cfg.Host))
+		host = cfg.Host
 	} else {
-		parts := strings.Split(cfg.Endpoint, ":")
-		if len(parts) != 2 {
-			return fmt.Errorf("malformed endpoint: %s", cfg.Endpoint)
-		}
-		log.Debug().Msgf("Sending traces using compact jaeger thrift protocol via udp into %s...", cfg.Endpoint)
-		// export via compact thrift protocol over upd - important
-		exp, err = jaeger.New(jaeger.WithAgentEndpoint(
-			jaeger.WithAgentHost(parts[0]),
-			jaeger.WithAgentPort(parts[1]),
-		))
+		host = loadFromEnv("OTEL_EXPORTER_JAEGER_AGENT_HOST", "localhost")
 	}
+	if cfg.Port != "" {
+		opts = append(opts, jaeger.WithAgentPort(cfg.Port))
+		port = cfg.Port
+	} else {
+		port = loadFromEnv("OTEL_EXPORTER_JAEGER_AGENT_PORT", "6831")
+	}
+	log.Debug().Msgf("Sending traces using compact jaeger thrift protocol via udp into %s:%s.", host, port)
+	exp, err = jaeger.New(jaeger.WithAgentEndpoint(opts...))
 	if err != nil {
 		return err
 	}
