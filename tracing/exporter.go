370453d09cfa21d1d36e0ea3179a6b9ad8eea912
Anatolij Ostroumov <anatolij@vodolaz095.ru>
2025-05-03 12:00:12 +0300

    tracing.Shutdown

--- tracing/exporter.go
+++ tracing/exporter.go
@@ -12,11 +12,27 @@ const defaultTimeout = 10 * time.Second
 
 var exp trace.SpanExporter
 
-// Wait allows span exporter to inhibit application shutdown until it sends all traces
+// Wait allows span exporter to inhibit application shutdown until it sends all traces.
 func Wait(ctx context.Context) (err error) {
 	if exp != nil {
 		<-ctx.Done()
 		shutdownContext, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+		defer cancel()
+		err = exp.Shutdown(shutdownContext)
+		if err != nil {
+			log.Error().Err(err).Msgf("Error saving telemetry spans: %s", err)
+			return err
+		}
+		log.Debug().Msgf("All spans are saved")
+		return nil
+	}
+	return nil
+}
+
+// Shutdown forces exporter to save all spans and turn off.
+func Shutdown(ctx context.Context) (err error) {
+	if exp != nil {
+		shutdownContext, cancel := context.WithTimeout(ctx, defaultTimeout)
 		defer cancel()
 		err = exp.Shutdown(shutdownContext)
 		if err != nil {
